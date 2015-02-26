5e1771c0ed296db94e1dddd340a7f8941402266b
金森幹斗 <kanamorimikito@kanamoritsuyoshito-no-MacBook-Pro.local>
2015-02-26 19:57:16 +0900

    Initial Commit

---
+++ ankiTests/ankiTests.m
@@ -0,0 +1,40 @@
+//
+//  ankiTests.m
+//  ankiTests
+//
+//  Created by 金森幹斗 on 2015/02/26.
+//  Copyright (c) 2015年 Mikito Kanamori. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface ankiTests : XCTestCase
+
+@end
+
+@implementation ankiTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
